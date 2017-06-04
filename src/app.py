61d3398c9c777a0d98d9e0226ee82c43d5319a0b
Rick <rickpowell1311@gmail.com>
2017-06-04 20:36:21 +0100

    Separate api actions into a separate module

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import actions.apiactions
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world'
+@app.route('/', methods = ['GET'])
+def home():
+    return actions.apiactions.get_home()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
