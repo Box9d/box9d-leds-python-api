1b5a2200b692bbd65b91eb0b3ed86590b3217a92
Rick <rickpowell1311@gmail.com>
2017-06-05 00:03:34 +0100

    Add py linting rules and update requirements.txt

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 import actions.apiactions
 
-app = Flask(__name__)
+APP = Flask(__name__)
 
-@app.route('/', methods = ['GET'])
+@APP.route('/', methods=['GET'])
 def home():
     return actions.apiactions.get_home()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    APP.run(debug=True, host='0.0.0.0')
